97679df0a282f36ee6c8be0e253e248714daba14
eduardoenemark <eduardoenemark@gmail.com>
2024-05-03 14:37:59 -0300

    add: enable-log-file and log-filename options.

--- examples/webscrapy-quotes.sh
+++ examples/webscrapy-quotes.sh
@@ -4,4 +4,6 @@ poetry run getallspider --url=https://quotes.toscrape.com \
                         --allowed-domains=toscrape.com \
                         --save-dir=./scrapy-quotes-webdomain \
                         --delay=0 \
+                        --enable-log-file=true \
+                        --log-filename=./quotes.out \
                         --also-save-links=true
