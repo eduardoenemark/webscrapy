94785096a0ec266b7668cd9d487852bd626dc755
eduardoenemark <eduardoenemark@gmail.com>
2024-05-02 23:18:57 -0300

    update: Accept-Language and Accept-Encoding value.

--- webscrapy/get_all_spider.py
+++ webscrapy/get_all_spider.py
@@ -142,8 +142,8 @@ def main():
         "DEFAULT_REQUEST_HEADERS": {
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "*",
+            "Accept-Encoding": "gzip, deflate,*/*",
             "Connection": "keep-alive",
             "Upgrade-Insecure-Requests": "1",
             "Sec-Fetch-Dest": "document",
